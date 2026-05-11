8e018a8825418599de42fc71fb7d786550ac11cc
Gigi-comp <gislainerodrigues1703@gmail.com>
2026-05-11 20:00:40 -0300

    enviando o terceiro filme

--- filmes.php
+++ filmes.php
@@ -5,7 +5,8 @@ session_start();
 if (!isset($_SESSION['filmes'])) {
     $_SESSION['filmes'] = [
         ["id" => 1, "titulo" => "Matrix", "ano" => 1999],
-        ["id" => 2, "titulo" => "Interestelar", "ano" => 2014]
+        ["id" => 2, "titulo" => "Interestelar", "ano" => 2014],
+        ["id" => 3, "titulo" => "A teoria de tudo", "ano" => 2014 ]
     ];
 }
 
